df842f136af52670186fb8b4efedd08f47c28a3a
Admin <Admin@DESKTOP-QOJI5AF.localdomain>
2024-11-22 23:47:00 +0530

    master 2nd commit

--- src/test/java/gittest23/test.java
+++ src/test/java/gittest23/test.java
@@ -5,6 +5,9 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("master first commit");
+		System.out.print("another1 first commit");
+		System.out.print("another1 second commit");
+		
 	}
 
 }
