6b7a33920894c46907a9321d144a312a07e16ab5
tester <ashwini.rao2105@gmail.com>
2024-11-23 00:01:30 +0530

    2nd commit

--- src/test/java/gittest23/test.java
+++ src/test/java/gittest23/test.java
@@ -5,6 +5,7 @@ public class test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.print("master first commit");
+		System.out.print("master 2nd commit");
 	}
 
 }
